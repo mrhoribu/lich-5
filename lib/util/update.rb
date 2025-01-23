9f18066f7386f993b34127f3e6616b939c52a2aa
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2025-01-23 16:59:08 -0500

    [util\update.rb] add Opts parsing to Update module

--- lib/util/update.rb
+++ lib/util/update.rb
@@ -3,6 +3,42 @@
 module Lich
   module Util
     module Update
+      module Opts
+        require 'ostruct'
+        
+        FLAG_PREFIX    = "--"
+    
+        def self.parse_command(h, c)
+          h[c.to_sym] = true
+        end
+
+        def self.parse_flag(h, f)
+          (name, val) = f[2..-1].split("=")
+          if val.nil?
+            h[name.to_sym] = true
+          else
+            val = val.split(",")
+
+            h[name.to_sym] = val.size == 1 ? val.first : val
+          end
+        end
+
+        def self.parse(args = Script.current.vars[1..-1])        
+          OpenStruct.new(**args.to_a.reduce(Hash.new) do |opts, v|
+            if v.start_with?(FLAG_PREFIX)
+              Opts.parse_flag(opts, v)
+            else
+              Opts.parse_command(opts, v)
+            end
+            opts
+          end)
+        end
+
+        def self.method_missing(method, *args)
+          parse.send(method, *args)
+        end
+      end
+
       require 'json'
       require 'open-uri'
       require 'rubygems/package'
