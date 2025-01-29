d4eeeae02eee523cea4357b5519e4254a2c5fe82
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2025-01-29 14:55:06 -0500

    rubocop

--- lib/common/frontend/warlock.rb
+++ lib/common/frontend/warlock.rb
@@ -7,17 +7,17 @@ module Lich
         @command_line_args = " --host localhost --port %port% --key %key%"
 
         def self.exist?
-          case Frontend.operating_system 
+          case Frontend.operating_system
           when :windows
             registry_path = "Software\\Classes\\Local Settings\\Software\\Microsoft\\Windows\\CurrentVersion\\AppModel\\Repository\\Packages"
-            possible_folder = Registry.open(Registry::HKEY_CURRENT_USER, registry_path).each_key.find  { |key| key.to_s =~ /Warlock/ }[0]
+            possible_folder = Registry.open(Registry::HKEY_CURRENT_USER, registry_path).each_key.find { |key| key.to_s =~ /Warlock/ }[0]
             return false if possible_folder.nil?
             folder_path = Registry.open(Registry::HKEY_CURRENT_USER, "#{registry_path}\\#{possible_folder}")['PackageRootFolder']
             if File.exist?(File.join(folder_path))
               @file_location = File.join(folder_path)
               @file_name = File.join("warlock3.exe")
               return true
-            end 
+            end
           when :macos
             if File.exist?(File.join("/Applications/Warlock3.app/Contents/MacOS/Warlock3"))
               @file_location = File.join("/Applications/Warlock3.app/Contents/MacOS")
